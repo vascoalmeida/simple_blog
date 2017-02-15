cf0f64b073acccad1a78dc0fa9cbf42163fa061a
vascoalmeida <castelobranco.vasco9@gmail.com>
2017-02-15 17:07:20 +0000

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    small_description = models.CharField(max_length=200)
-
+    
     def publish(self):
         self.published_date = timezone.now()
         self.save()
